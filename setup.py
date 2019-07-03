70930dbba53aee4777a94a94e12f16da3af4dd59
agamemnonc <agamemnon.krasoulis@gmail.com>
2019-07-03 18:55:39 +0100

    remove dependency for matplotlib > 3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ INSTALL_REQUIRES = [
     'numpy>=1.15',
     'scipy>=1.2',
     'pandas>=0.23',
-    'matplotlib>=3.0.2',
+    'matplotlib>=2.2.2',
     'seaborn>=0.9.0',
     'statsmodels>=0.10.0'
 ]
